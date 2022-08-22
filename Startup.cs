5be5639582d03f1f0f47ce60eb93d75ef83bbecf
Kirk Davis <kirkdavi@amazon.com>
2022-08-22 15:50:52 -0700

    commented out https redirect to make local demo easier

--- Startup.cs
+++ Startup.cs
@@ -37,9 +37,10 @@ namespace FargateFromAzureDevOps
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();  commenting out to allow developers to run locally w/o browser issues.
+
             app.UseStaticFiles();
 
             app.UseRouting();
